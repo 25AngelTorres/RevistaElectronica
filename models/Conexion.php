93ef1a5c7490c8f924100663f00a8c7275842cec
25AngelTorres <25AngelTorres@gmail.com>
2014-08-11 01:15:42 -0500

    purga3

--- models/Conexion.php
+++ models/Conexion.php
@@ -16,13 +16,8 @@ class Conexion {
         $this->db = ADONewConnection('mysql');
         $this->db->debug = false;
 			       //ip     user      pass    bd
-<<<<<<< HEAD
-        $this->db->Connect('localhost','RevistaProyecto','1234RP','revista');
-		//$this->db->Connect('localhost','ale','1234','revista');
 
-=======
-       $this->db->Connect('localhost','RevistaProyecto','1234RP','revista');
->>>>>>> 0c5662ee4d437e2b026bf7f59503a2557c91835f
+        $this->db->Connect('localhost','RevistaProyecto','1234RP','revista');
     }
     
 }

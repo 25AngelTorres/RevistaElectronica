938122211ba144cf3343eb7142325e08a38cc557
Oscar Grimaldo Aguayo <oscar.grimaldo@itcelaya.edu.mx>
2014-08-11 13:15:54 -0500

    textarea con imagenes

--- views/upload/upload.php
+++ views/upload/upload.php
@@ -1,26 +1,15 @@
-<pre>
-Array
-(
-    [file] => Array
-        (
-            [name] => wallpaper-94255.jpg
-            [type] => image/jpeg
-            [tmp_name] => /tmp/phpaNux1a
-            [error] => 0
-            [size] => 1028697
-        )
-
-)
-
 <?php
-print_r($_FILES);
-print_r($_POST);
-print_r($_GET);
+//print_r($_FILES);
+//print_r($_POST);
+//print_r($_GET);
 
 if(move_uploaded_file($_FILES['file']['tmp_name'], "".$_FILES['file']['name'])){
-	echo "archivo actulizado correctamento";
+	//echo "archivo actulizado correctamento";
 }else{
 	echo "Error en upload file";
 }
+$imagen = array( 'link' => 'http://localhost/RevistaElectronica/views/upload/'.$_FILES['file']['name'] ); 
+
+echo json_encode($imagen);
 
 ?>
